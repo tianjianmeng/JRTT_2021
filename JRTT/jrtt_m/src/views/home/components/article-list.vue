d6ff0997b39b24f2ae03e959db7df1fadcc692af
扭曲树黑色精子 <1923215589@qq.com>
2021-10-23 10:53:11 +0800

    Update article-list.vue

--- JRTT/jrtt_m/src/views/home/components/article-list.vue
+++ JRTT/jrtt_m/src/views/home/components/article-list.vue
@@ -50,6 +50,7 @@ export default {
       // 1. 请求获取数据
       const { data } = await getArticle()
 
+
       // 2. 将数据添加到当前频道的文章列表中
       // ...数组，数组的展开操作符，它会把数组元素一个一个的拿出来，传递给使用的位置
       const { result } = data
@@ -60,34 +61,22 @@ export default {
       // 3. 将 loading 设置为 false
       this.loading = false
       this.finished = true
-      // 4. 判断还有下一页数据，则更新获取下一个数据的时间戳
-      //    如果没有了，则将 finished 设置为 true，不再加载更多了
-      // if (results.length) {
-      //   // 更新获取下一页数据的时间戳
-      //   this.timestamp = data.data.pre_timestamp
-      // } else {
-      //   this.finished = true
-      // }
-    },
-
+    }
+    ,
     async onRefresh () {
       // 1. 请求获取最新数据
-      const { data } = await getArticles({
-        channel_id: this.channel.id,
-        timestamp: Date.now(), // 获取最新数据传递当前最新时间戳即可
-        with_top: 1
-      })
+      const { data } = await getArticle()
 
       // 2. 把数据放到列表的顶部
-      const results = data.data.results
-      this.list.unshift(...results)
+      const { result } = data
+      this.list.unshift(...result)
 
       // 3. 停止下拉刷新的转圈圈
       this.refreshLoading = false
-
+      //this.finished = true
       // 4. 提示用户刷新成功
-      const message = results.length
-        ? `更新了${results.length}条数据`
+      const message = result.length
+        ? `更新了${result.length}条新闻`
         : '暂无数据更新'
       this.$toast(message)
     }
